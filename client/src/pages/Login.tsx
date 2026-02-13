af53a255e42ada05b7920309081d395c36e37c91
CorentinNOC <c.nicolas28210@gmail.com>
2026-02-13 15:18:39 +0100

    feat(login): integration login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,10 @@
 const Login = () => {
   return (
     <main className="h-screen flex items-center justify-center">
-      <form className="w-full max-w-[380px] rounded-lg p-6 bg-gray-200 flex flex-col">
+      <form className="w-full max-w-[380px] rounded-lg p-6 border border-secondary flex flex-col">
         <label htmlFor="mail">E-mail</label>
         <input
-          className="bg-gray-300 my-2 py-2 px-4"
+          className="border border-secondary my-2 py-2 px-4"
           type="email"
           id="mail"
           name="mail"
@@ -13,7 +13,7 @@ const Login = () => {
         />
         <label htmlFor="password">Mot de passe</label>
         <input
-          className="bg-gray-300 my-2 py-2 px-4"
+          className="border border-secondary my-2 py-2 px-4"
           id="password"
           name="password"
           type="password"
