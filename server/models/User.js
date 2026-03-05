31a79a0902d3ffde7f9e7f1ec9fd2ad27e89ee19
CorentinNOC <c.nicolas28210@gmail.com>
2026-03-05 10:21:40 +0100

    fix(dependencies: remove deprecated depedencies

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-userSchema.plugin(uniqueValidator);
+userSchema.index({ email: 1 }, { unique: true });
 
 module.exports = mongoose.model("User", userSchema);
