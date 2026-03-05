cb941d2c03f00ff32291de1746d4f06cba4a6836
CorentinNOC <c.nicolas28210@gmail.com>
2026-03-05 09:52:57 +0100

    feat(deployment): setup for deployment

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
@@ -9,6 +10,13 @@ const userRoutes = require("./routes/auth");
 const path = require("path");
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://corentinnicolas.vercel.app"],
+    credentials: true,
+  }),
+);
 
 mongoose
   .connect(process.env.MONGO_URI)
