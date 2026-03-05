9f17a82fa0126f5f10444230fec0e8e2a4883f45
CorentinNOC <c.nicolas28210@gmail.com>
2026-03-05 10:56:11 +0100

    Fix CORS configuration order

--- server/app.js
+++ server/app.js
@@ -1,43 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
-const app = express();
-
-const projectRoutes = require("./routes/project");
-const experienceRoutes = require("./routes/experience");
-const userRoutes = require("./routes/auth");
 const path = require("path");
+require("dotenv").config();
 
-app.use(express.json());
+const app = express();
 
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://portfolio-drab-nine-4fsye5z9gd.vercel.app/",
+      "https://portfolio-drab-nine-4fsye5z9gd.vercel.app",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch((err) => console.log("Connexion à MongoDB échouée !", err));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-  );
-  next();
-});
 
 app.get("/robots.txt", (req, res) => {
   res.type("text/plain");
@@ -47,9 +36,12 @@ Disallow: /dashboard
 Disallow: /login`);
 });
 
+const projectRoutes = require("./routes/project");
+const experienceRoutes = require("./routes/experience");
+const userRoutes = require("./routes/auth");
+
 app.use("/api/projects", projectRoutes);
 app.use("/api/experiences", experienceRoutes);
 app.use("/api/auth", userRoutes);
-app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
